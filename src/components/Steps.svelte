348260b7be2b52f1d927130b8003bcc85438ccd0
EmNudge <emnudge@gmail.com>
2020-12-30 19:03:18 -0500

    added that thing like in the spec where you click variables

--- src/components/Steps.svelte
+++ src/components/Steps.svelte
@@ -3,9 +3,35 @@
   import Step from './Step.svelte';
 
   import { stepArr } from '../stores';
+
+  const X_COLOR = 'var.token-x { background: var(--highlight-color); }';
+  const Y_COLOR = 'var.token-y { background: var(--highlight-color); }';
+  let currColor = '';
+  $: style = `<style>${currColor}</style>`;
+
+  function handleClick(e) {
+    const { className } = e.target;
+    if (!className.includes('token')) return;
+
+    if (className.includes('-x')) {
+      if (currColor === X_COLOR) {
+        currColor = '';
+      } else {
+        currColor = X_COLOR;
+      }
+    } else {
+      if (currColor === Y_COLOR) {
+        currColor = '';
+      } else {
+        currColor = Y_COLOR;
+      }
+    }
+  }
 </script>
 
-<ol>
+{@html style}
+
+<ol on:click={handleClick} {style}>
   {#each steps as step, i}
     <Step {step} stepArr={$stepArr} index={i} />
   {/each}
--- src/utils/parsemd.ts
+++ src/utils/parsemd.ts
@@ -1,6 +1,12 @@
+// turns x and y into tokens for later
+const maybeTokenize = (str: string) => str.replace(
+  /(^|\(| )([xy])( |\)|$)/g, 
+  `$1<var class="token-$2">$2</var>$3`
+);
+
 export function parse(substep: string): string {
   return substep.replace(
     /`(.+?)`/g, 
-    '<code>$1</code>'
+    (_match, code) => `<code>${maybeTokenize(code)}</code>`
   );
 }
